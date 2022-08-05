02b42339aee888d51b39e69b2634a7f62b57eac2
Damir <goblin11111016@gmail.com>
2022-08-05 09:58:24 -0400

    Update ERC20 Funge Test Case

--- test/TestFunge.js
+++ test/TestFunge.js
@@ -8,7 +8,7 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 describe("Testing of the Funge ERC20 Contract", function () {
 
     async function deployFungeFixture() {
-        const [owner, otherAccount] = await ethers.getSigners();
+        const [owner, otherAccount, addr1, addr2] = await ethers.getSigners();
 
         const _mintable = true;
         const _name = "Funge";
@@ -28,7 +28,7 @@ describe("Testing of the Funge ERC20 Contract", function () {
         await Funge.initialize(_name, _symbol, _decimals, _initialSupply, _mintable, owner.address);
         // console.log("Funge contract address:", Funge.address);
 
-        return { Funge, owner, otherAccount };
+        return { Funge, owner, otherAccount, _mintable, addr1, addr2 };
     }
 
     describe("Deployment", function () {
@@ -36,6 +36,90 @@ describe("Testing of the Funge ERC20 Contract", function () {
             const { Funge, owner } = await loadFixture(deployFungeFixture);
             expect(await Funge.getOwner()).to.equal(owner.address);
         });
+        it("Should set the mintable", async function () {
+            const { Funge, owner, _mintable } = await loadFixture(deployFungeFixture);
+            expect(await Funge.mintable()).to.equal(_mintable);
+        });
+        it("Should assign the total supply of tokens to the owner", async function () {
+            const { Funge, owner } = await loadFixture(deployFungeFixture);
+            const ownerBalance = await Funge.balanceOf(owner.address);
+            expect(await Funge.totalSupply()).to.equal(ownerBalance);
+        });
+    });
+
+    describe("Transactions", function () {
+        it("Should transfer tokens between accounts", async function () {
+          const { Funge, owner, addr1, addr2 } = await loadFixture(
+            deployFungeFixture
+          );
+          // Transfer 50 tokens from owner to addr1
+          await expect(
+            Funge.transfer(addr1.address, 50)
+          ).to.changeTokenBalances(Funge, [owner, addr1], [-50, 50]);
+    
+          // Transfer 50 tokens from addr1 to addr2
+          // We use .connect(signer) to send a transaction from another account
+          await expect(
+            Funge.connect(addr1).transfer(addr2.address, 50)
+          ).to.changeTokenBalances(Funge, [addr1, addr2], [-50, 50]);
+        });
+    
+        it("should emit Transfer events", async function () {
+          const { Funge, owner, addr1, addr2 } = await loadFixture(
+            deployFungeFixture
+          );
+    
+          // Transfer 50 tokens from owner to addr1
+          await expect(Funge.transfer(addr1.address, 50))
+            .to.emit(Funge, "Transfer")
+            .withArgs(owner.address, addr1.address, 50);
+    
+          // Transfer 50 tokens from addr1 to addr2
+          // We use .connect(signer) to send a transaction from another account
+          await expect(Funge.connect(addr1).transfer(addr2.address, 50))
+            .to.emit(Funge, "Transfer")
+            .withArgs(addr1.address, addr2.address, 50);
+        });
+    
+        it("Should fail if sender doesn't have enough tokens", async function () {
+          const { Funge, owner, addr1 } = await loadFixture(
+            deployFungeFixture
+          );
+          const initialOwnerBalance = await Funge.balanceOf(owner.address);
+    
+          // Try to send 1 token from addr1 (0 tokens) to owner (1000 tokens).
+          // `require` will evaluate false and revert the transaction.
+          await expect(
+            Funge.connect(addr1).transfer(owner.address, 1)
+          ).to.be.reverted;
+    
+          // Owner balance shouldn't have changed.
+          expect(await Funge.balanceOf(owner.address)).to.equal(
+            initialOwnerBalance
+          );
+        });
+        it("Should update balances after transfers", async function () {
+            const { Funge, owner, addr1, addr2 } = await loadFixture(
+                deployFungeFixture
+            );
+			const initialOwnerBalance = await Funge.balanceOf(owner.address);
+
+			// Transfer 100 tokens from owner to addr1.
+			await Funge.transfer(addr1.address, 100);
+
+			// Transfer another 50 tokens from owner to addr2.
+			await Funge.transfer(addr2.address, 50);
+
+			// Check balances.
+			const finalOwnerBalance = await Funge.balanceOf(owner.address);
+			expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
+
+			const addr1Balance = await Funge.balanceOf(addr1.address);
+			expect(addr1Balance).to.equal(100);
+
+			const addr2Balance = await Funge.balanceOf(addr2.address);
+			expect(addr2Balance).to.equal(50);
+		});
     });
 
 });
--- test/TestVesting.js
+++ test/TestVesting.js
@@ -34,12 +34,24 @@ describe("Testing of the Funge Vesting Contract", function () {
         await Funge.initialize(_name, _symbol, _decimals, _initialSupply, _mintable, FungeVesting.address);
         await FungeVesting.initialize(FungeAddress);
 
-        
+        const _beneficiary = ethers.utils.getAddress("0x1155E6509bBd1EB643800681C264F03a0De6AfdE");
+        const _start = ethers.BigNumber.from(1659460043);
+        const _cliff = 1;
+        const _duration = ethers.BigNumber.from(31556926);
+        const _slicePeriodSeconds = 1;
+        const _revocable = true;
+        const _amount = ethers.BigNumber.from("10000000000000000000");
+
+        await FungeVesting.createVestingSchedule(_beneficiary, _start, _cliff, _duration, _slicePeriodSeconds, _revocable, _amount);
+        await FungeVesting.createVestingSchedule(_beneficiary, _start, _cliff, _duration, _slicePeriodSeconds, false, _amount);
+
+        const tempSchedule = await FungeVesting.getVestingIdAtIndex(0);
+        const temp1Schedule = await FungeVesting.getVestingIdAtIndex(1);
 
         // console.log("FungeVesting contract address:", FungeVesting.address);
         // console.log("Balance of the FungeVesting:", await Funge.balanceOf(FungeVesting.address));
 
-        return { FungeVesting, owner, otherAccount, FungeAddress };
+        return { FungeVesting, owner, otherAccount, FungeAddress, tempSchedule, temp1Schedule, Funge };
     }
 
     describe("Deployment", function () {
@@ -57,7 +69,7 @@ describe("Testing of the Funge Vesting Contract", function () {
     });
 
     describe("Create Vesting Schdule", function () {
-        it("Owner can only add the vesting schedule data", async function () {
+        it("Only owner can add the vesting schedule data", async function () {
 
             const _beneficiary = ethers.utils.getAddress("0x1155E6509bBd1EB643800681C264F03a0De6AfdE");
             const _start = ethers.BigNumber.from(1659460043);
@@ -71,19 +83,73 @@ describe("Testing of the Funge Vesting Contract", function () {
             await expect(FungeVesting.connect(otherAccount)
             .createVestingSchedule(_beneficiary, _start, _cliff, _duration, _slicePeriodSeconds, _revocable, _amount)).to.be.revertedWith("Ownable: caller is not the owner");
         });
+
+        // it("FungeVesting cannot create vesting schedule because not sufficient tokens", async function () {
+
+        //     const _beneficiary = ethers.utils.getAddress("0x1155E6509bBd1EB643800681C264F03a0De6AfdE");
+        //     const _start = ethers.BigNumber.from(1659460043);
+        //     const _cliff = 10;
+        //     const _duration = ethers.BigNumber.from(31556926);
+        //     const _slicePeriodSeconds = 10;
+        //     const _revocable = true;
+        //     const _amount = ethers.BigNumber.from("10000000000000000000");
+        //     const { FungeVesting, owner, otherAccount, FungeAddress, Funge } = await loadFixture(deployFungeVestingFixture);
+
+        //     await expect(FungeVesting
+        //     .createVestingSchedule(_beneficiary, _start, _cliff, _duration, _slicePeriodSeconds, _revocable, _amount))
+        //     .to.be.revertedWith("FungeVesting: cannot create vesting schedule because not sufficient tokens");
+        // });
     });
 
     describe("Release Vesting Schdule", function () {
-        it("Owner can only release the vesting schedule to each users", async function () {
+        it("Only owner can release the vesting schedule to each users", async function () {
 
             const { FungeVesting, owner, otherAccount, FungeAddress } = await loadFixture(deployFungeVestingFixture);
 
-            // const vestingSchduleId = await FungeVesting.
-            
-
             await expect(FungeVesting.connect(otherAccount)
-            .createVestingSchedule(_beneficiary, _start, _cliff, _duration, _slicePeriodSeconds, _revocable, _amount)).to.be.revertedWith("Ownable: caller is not the owner");
+            .releaseAll()).to.be.revertedWith("Ownable: caller is not the owner");
         });
     });
+
+    describe("Revoke Vesting Schdule", function () {
+        it("Only owner can release the vesting schedule to each users", async function () {
+
+            const { FungeVesting, owner, otherAccount, FungeAddress, tempSchedule} = await loadFixture(deployFungeVestingFixture);
+
+            await expect(FungeVesting.connect(otherAccount)
+            .revoke(tempSchedule)).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+
+        it("Whether or not the Vesting Schedule can revoke", async function () {
+
+            const { FungeVesting, owner, otherAccount, FungeAddress, temp1Schedule} = await loadFixture(deployFungeVestingFixture);
+
+            await expect(FungeVesting
+            .revoke(temp1Schedule)).to.be.revertedWith("FungeVesting: vesting is not revocable");
+        });
+    });
+
+    describe("Withdraw the Funge", function () {
+        it("Only owner can withdraw the specified amount of Funge Token", async function () {
+
+            const amount = 10;
+
+            const { FungeVesting, owner, otherAccount, FungeAddress, tempSchedule } = await loadFixture(deployFungeVestingFixture);
+
+            await expect(FungeVesting.connect(otherAccount)
+            .withdraw(amount)).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+
+        it("FungeVesting have not enough withdrawable funds", async function () {
+
+            const { FungeVesting, owner, otherAccount, FungeAddress, tempSchedule, Funge } = await loadFixture(deployFungeVestingFixture);
+
+            const amount = Funge.balanceOf(FungeVesting.address);
+
+            await expect(FungeVesting
+            .withdraw(amount)).to.be.revertedWith("FungeVesting: not enough withdrawable funds");
+        });
+    });
+
     
 });
